d658dde6637bae1b5d66834831140bd83096fc4f
user <user@user-PC2-Aer-DS-5250RYK-Series.(none)>
2021-08-05 15:50:14 +0200

    Modyfikacja pola czas

--- szkolenie/modelszkolenie/admin.py
+++ szkolenie/modelszkolenie/admin.py
@@ -9,11 +9,3 @@ admin.site.register(Answer)
 admin.site.register(GaleryImage)
 admin.site.register(QuestionImage)
 
-
-class TrainingAdmin(admin.ModelAdmin):
-    list_display = ('_my_field',)
-    readonly_fields = ('_my_field', )     
-
-    def _my_field(self, obj):
-        return obj.get_full_name()
-    _my_field.short_description = 'my custom label'
