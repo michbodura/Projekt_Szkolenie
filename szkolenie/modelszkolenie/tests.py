9dd23a020b1f7cef066e7ce057918ddef793634e
user <user@user-PC2-Aer-DS-5250RYK-Series.(none)>
2021-08-06 09:49:46 +0200

    Dodanie testu ifUserEquals

--- szkolenie/modelszkolenie/tests.py
+++ szkolenie/modelszkolenie/tests.py
@@ -16,11 +16,35 @@ class UserTestCase(TestCase):
         user1 = User.objects.get(imie2="Michal")
         user2 = User.objects.get(email2="piotr.nowak@iutechnology.pl")
     
-    def ifPiotrNowakExists(self):
+    def test_ifPiotrNowakExists(self):
         User.objects.create(imie="Piotr", nazwisko="Nowak")
         ex1 = User.objects.get(imie="Piotr", nazwisko="Nowak")
 
+    def test_ifMichalInTraining(self):
+        user1 = User.objects.create(imie="Michal", nazwisko="Bodura")
+        user2 = User.objects.create(imie="Michal", nazwisko="Bodura")
+        queryset = User.objects.filter(imie="Michal2")
+        print(queryset)
+
+    # False
+    def test_ifUsersEquals_one(self):
+        user1 = User.objects.create(imie="Michal", nazwisko="Bodura")
+        user2 = User.objects.create(imie="Michal", nazwisko="Bodura")
+        self.assertEquals(user1,user2)
+
+    # Czy zachowana zostala unikalnosc w bazie danych
+    def test_ifUsersEquals_one(self):
+        user1 = User.objects.create(imie="Michal", nazwisko="Bodura")
+        user2 = User.objects.create(imie="Michal", nazwisko="Bodura")
+        self.assertEquals(user1,user2)
+     
+    def test_ifUsersEquals_two(self):
+        user1 = User.objects.create(id=6, imie="Michal", nazwisko="Bodura",email="michal_bodura@iutechnology.pl", nrDowodu="CDJ757557", jezyk="EN")
+        user2 = User.objects.create(id=7, imie="Michal", nazwisko="Bodura",email="michal_bodura@iutechnology.pl", nrDowodu="CDJ757557", jezyk="EN")
+        self.assertEquals(user1,user2)
     
+    
+
 
 
 
