dafde2cd480ef2fb1b3b2ac301e0c5013972b593
user <user@user-PC2-Aer-DS-5250RYK-Series.(none)>
2021-08-06 10:02:05 +0200

    Dodanie testu if user and company not equals

--- szkolenie/modelszkolenie/tests.py
+++ szkolenie/modelszkolenie/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from modelszkolenie.models import User
+from modelszkolenie.models import Company, User
 
 class UserTestCase(TestCase):
     def setUp(self):
@@ -21,21 +21,25 @@ class UserTestCase(TestCase):
         ex1 = User.objects.get(imie="Piotr", nazwisko="Nowak")
 
     def test_ifMichalInTraining(self):
-        user1 = User.objects.create(imie="Michal", nazwisko="Bodura")
-        user2 = User.objects.create(imie="Michal", nazwisko="Bodura")
-        queryset = User.objects.filter(imie="Michal2")
-        print(queryset)
+        self.UserTestCase.setUp()
+        queryset = User.objects.filter(imie="Michal")
 
     # False
     def test_ifUsersEquals_one(self):
         user1 = User.objects.create(imie="Michal", nazwisko="Bodura")
         user2 = User.objects.create(imie="Michal", nazwisko="Bodura")
         self.assertEquals(user1,user2)
-     
-    def test_ifUsersEquals_two(self):
-        user1 = User.objects.create(id=6, imie="Michal", nazwisko="Bodura",email="michal_bodura@iutechnology.pl", nrDowodu="CDJ757557", jezyk="EN")
-        user2 = User.objects.create(id=7, imie="Michal", nazwisko="Bodura",email="michal_bodura@iutechnology.pl", nrDowodu="CDJ757557", jezyk="EN")
-        self.assertEquals(user1,user2)
+     # True
+    def test_ifUsersNotEquals(self):
+        user1 = User.objects.create(imie="Michal", nazwisko="Bodura",email="michal_bodura@iutechnology.pl", nrDowodu="CDJ757557", jezyk="EN")
+        user2 = User.objects.create(imie="Michal", nazwisko="Bodura",email="michal_bodura@iutechnology.pl", nrDowodu="CDJ757557", jezyk="EN")
+        self.assertNotEquals(user1,user2)
+
+class CompanyTestCase(TestCase):
+    def test_ifNotEquals(self):
+        company1 = Company.objects.create(nazwa="IU Technology", adres="Dubois 114/116")
+        company2 = Company.objects.create(nazwa="IU Technology", adres="Dubois 112")
+        self.assertNotEquals(company1,company2)
     
 
 
