e6e100c437cfd026a600fc5b4708b424509439c0
user <user@user-PC2-Aer-DS-5250RYK-Series.(none)>
2021-08-06 09:15:25 +0200

    Dodanie dwoch testow jednostkowych2

--- szkolenie/modelszkolenie/tests.py
+++ szkolenie/modelszkolenie/tests.py
@@ -6,15 +6,17 @@ class UserTestCase(TestCase):
         User.objects.create(imie="Michal", nazwisko="Bodura")
         User.objects.create(email="piotr.nowak@iutechnology.pl")
     
-    # Sprawdzenie poprawnosci modelu pod wzgledem atrybutu
+    # Sprawdzenie poprawnosci modelu pod wzgledem atrybutu - poprawna nazwa atrybutu
     def test_attributes(self):
         user1 = User.objects.get(imie="Michal")
         user2 = User.objects.get(email="piotr.nowak@iutechnology.pl")
     
-    # Sprawdzanie nazwy atrybutu
+    # Sprawdzanie nazwy atrybutu - bledna nazwa atrybutu
     def test_attributes2(self):
         user1 = User.objects.get(imie2="Michal")
-        user2 = User.objects.get(email2="piotrnowak@iutechnology.pl")
+        user2 = User.objects.get(email2="piotr.nowak@iutechnology.pl")
+    
+    
 
 
 # Create your tests here.
